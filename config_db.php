b3b6412df25a3c411649cb901328c29c82175820
MohamedHamouch <med10hamouch@gmail.com>
2024-12-17 16:32:31 +0100

    database procedural connextion

--- config_db.php
+++ config_db.php
@@ -5,7 +5,7 @@ $db_pass = "0000";
 $db_name = "blogDB";
 $conn = "";
 
-$conn = mysqli_connect($db_server, $db_user, $dbpass, $db_name);
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 if (mysqli_connect_errno()) {
 
   die("Connection failed: " . mysqli_connect_error());
