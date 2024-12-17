9f8fd24d8e902374628482c8a315df224e39bdab
MohamedHamouch <med10hamouch@gmail.com>
2024-12-17 12:15:03 +0100

    connect to the database

---
+++ config_db.php
@@ -0,0 +1,12 @@
+<?php
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "0000";
+$db_name = "blogDB";
+$conn = "";
+
+$conn = mysqli_connect($db_server, $db_user, $dbpass, $db_name);
+if (mysqli_connect_errno()) {
+
+  die("Connection failed: " . mysqli_connect_error());
+}
